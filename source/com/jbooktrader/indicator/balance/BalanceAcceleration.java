7a73c4b14be29e13c4a6570da92cd61e5bc58210
eugene.kononov <eugene.kononov@localhost>
2013-01-10 17:02:13 -0500

    Fixed typos.

--- source/com/jbooktrader/indicator/balance/BalanceAcceleration.java
+++ source/com/jbooktrader/indicator/balance/BalanceAcceleration.java
@@ -6,14 +6,14 @@ import com.jbooktrader.platform.indicator.*;
  * Balance acceleration
  */
 public class BalanceAcceleration extends Indicator {
-    private final double fastMultiplier, intermMutiplier, slowMultiplier;
+    private final double fastMultiplier, intermMultiplier, slowMultiplier;
     private double fast, slow, interm;
 
     public BalanceAcceleration(int period, int mult) {
         super(period, mult);
         fastMultiplier = 2.0 / (period + 1);
         double multiplier = mult / 10.0;
-        intermMutiplier = 2.0 / (multiplier * period + 1);
+        intermMultiplier = 2.0 / (multiplier * period + 1);
         slowMultiplier = 2.0 / (2 * multiplier * period + 1);
     }
 
@@ -21,7 +21,7 @@ public class BalanceAcceleration extends Indicator {
     public void calculate() {
         double balance = marketBook.getSnapshot().getBalance();
         fast += (balance - fast) * fastMultiplier;
-        interm += (balance - interm) * intermMutiplier;
+        interm += (balance - interm) * intermMultiplier;
         slow += (balance - slow) * slowMultiplier;
         value = fast - 2 * interm + slow;
     }
